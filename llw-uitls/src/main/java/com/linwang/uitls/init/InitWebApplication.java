9f0bf04efaef283705a57120489e3ac0da275b51
曹开魁 <caokaikui@51nbapi.com>
2017-04-04 14:21:36 +0800

    添加初始参数
    Signed-off-by: 曹开魁 <caokaikui@51nbapi.com>

--- llw-uitls/src/main/java/com/linwang/uitls/init/InitWebApplication.java
+++ llw-uitls/src/main/java/com/linwang/uitls/init/InitWebApplication.java
@@ -7,12 +7,15 @@ import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
+import main.java.com.UpYun;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import com.google.common.base.Strings;
 import com.linwang.uitls.StaticProp;
 import com.linwang.uitls.idsquence.SequenceUtils;
 
@@ -33,26 +36,26 @@ public class InitWebApplication {
 		if (dataSourceMaster != null) {
 			SequenceUtils.setDataSource(dataSourceMaster);
 		}
-//		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
-//		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
-//		String upYunFold = StaticProp.sysConfig.get("upyun.fold");
-//		if (!Strings.isNullOrEmpty(upYunFold)) {
-//			upYunFold += "/";
-//		}
-//		StaticProp.upYunPath = upYunFold + format.format(new Date()) + "/";
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+		String upYunFold = StaticProp.sysConfig.get("upyun.fold");
+		if (!Strings.isNullOrEmpty(upYunFold)) {
+			upYunFold += "/";
+		}
+		StaticProp.upYunPath = upYunFold + format.format(new Date()) + "/";
 		System.setProperty("org.freemarker.loggerLibrary", "none");
 		try {
 			//又拍云开关
-//			String upyunOpen = StaticProp.sysConfig.get("upyun.open");
-//			if(Strings.isNullOrEmpty(upyunOpen) || upyunOpen.equalsIgnoreCase("off")){
-//				StaticProp.IS_USER_UPYUN = false;
-//			}else{
-//				StaticProp.UP_YUN = new UpYun(
-//						StaticProp.sysConfig.get("upyun.bucketName"), 
-//						StaticProp.sysConfig.get("upyun.userName"), 
-//						StaticProp.sysConfig.get("upyun.password"));
-//				StaticProp.UP_YUN.setApiDomain(UpYun.ED_AUTO);
-//			}
+			String upyunOpen = StaticProp.sysConfig.get("upyun.open");
+			if(Strings.isNullOrEmpty(upyunOpen) || upyunOpen.equalsIgnoreCase("off")){
+				StaticProp.IS_USER_UPYUN = false;
+			}else{
+				StaticProp.UP_YUN = new UpYun(
+						StaticProp.sysConfig.get("upyun.bucketName"), 
+						StaticProp.sysConfig.get("upyun.userName"), 
+						StaticProp.sysConfig.get("upyun.password"));
+				StaticProp.UP_YUN.setApiDomain(UpYun.ED_AUTO);
+			}
 			StaticProp.cookieID = StaticProp.sysConfig.get("cookie_id");
 			StaticProp.SYSTEM = LogManager.getLogger("SYSLOG");
 		} catch (Exception e) {
